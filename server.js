2c559029f5d7b2acd70c9a6dddf767b800e51f36
David Williams <davwil00@gmail.com>
2024-11-26 20:27:30 +0000

    Filter to allowed gist ids

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const port = process.env.PORT || 8002
+const allowedGistIds = ['ae9400cb85f7f1a801e5331955f16382']
 
 if (!process.env.GITHUB_TOKEN) {
   console.error('No github token provided, please set GITHUB_TOKEN env var')
@@ -12,6 +13,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { response } = require('express')
+const {all} = require("express/lib/application");
 
 const app = express()
 app.use(bodyParser.json())
@@ -26,6 +28,9 @@ app.options('/:gistId', cors(corsOptions))
 
 app.patch('/:gistId', cors(corsOptions), async (req, res) => {
   const gistId = req.params.gistId
+  if (!allowedGistIds.includes(gistId)) {
+    res.sendStatus(401)
+  }
   const data = req.body
   const response = await patchGist(gistId, data)
   res.sendStatus(response.status, response.body)
